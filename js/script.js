e9eba9d02397822d767c4894290cea1da1f67474
Jakub Szczechowicz <jakub.szczechowicz@gmail.com>
2022-01-10 20:24:30 +0100

    adding tasks

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@
 
         for(const task of tasks) {
             htmlString += `
-                <li>
+                <li ${task.done ? "class=\"taskList__item--done\"" : "class=\"taskList__item\""}>
                     ${task.content}
                 </li>
             `;
@@ -24,10 +24,36 @@
         document.querySelector(".js-taskList").innerHTML = htmlString;
     }
 
+    const addNewTask = (taskContent) => {
+        tasks.push({
+            content: taskContent,
+        });
+
+        render();
+    }
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+        
+        if(newTaskContent === ""){
+            return;
+        }
+
+        addNewTask(newTaskContent);
+
+        document.querySelector(".js-newTask").value = "";
+    };
+
 
     const init = () => {
         console.log("Initializing");
         render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
     }
 
     init();
